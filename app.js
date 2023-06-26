02d3da9f5072a7b8c71f49adb548a53a665bc0fb
coti-tech <eli@coti.io>
2023-06-26 18:47:39 +0300

    fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const {
 // chose coti network , mainnet or testnet
 const network = 'mainnet';
 const fullNodeUrl = 'full node url';
-const trustScoreNodeUrl = 'trust score url'
+const trustScoreNodeUrl = 'trust score url';
 
 // api key that Coti provides, only to be used one time for each seed to insert the public key to the trust score node
 const apiKey = 'yourApiKey';
@@ -30,7 +30,7 @@ const apiKey = 'yourApiKey';
 
     // generate seed in 32bytes
     const seed = cryptoUtils.generateSeed(seedIn64Bytes);
-    console.log(`seedIn32Bytes: ${seed}`)
+    console.log(`seedIn32Bytes: ${seed}`);
 
     // ec to be used to sign all messages
     const userKeyPair = new EcKeyPair(seed);
@@ -53,23 +53,23 @@ const apiKey = 'yourApiKey';
       console.log(trustScoreResponse);
     } catch (e) {
       if (e.message === 'User does not exist!') {
-        // seeting trust score. Only one time action.
+        // setting trust score. Only one time action.
         const trustScoreResponse = await nodeUtils.setTrustScore(apiKey, userHash, network);
         console.log(`Trust score inserted:`);
         console.log(trustScoreResponse);
       }
     }
 
-    // BASEWALLET and WEBSOCKET use
-    const baseWallet = new Wallet({ seed, fullnode: fullNodeUrl, network, trustScoreNode: trustScoreNodeUrl });
+    // WALLET and WEBSOCKET use
+    const wallet = new Wallet({ seed, fullnode: fullNodeUrl, network, trustScoreNode: trustScoreNodeUrl });
     // listen for balance changes
-    baseWallet.onBalanceChange((balanceChangedAddress) => console.log('Balance change: ', balanceChangedAddress));
+    wallet.onBalanceChange((balanceChangedAddress) => console.log('Balance change: ', balanceChangedAddress));
     //listen for new transaction or transaction status changes
-    baseWallet.onReceivedTransaction((receivedTransaction) => console.log('Received transaction', receivedTransaction));
+    wallet.onReceivedTransaction((receivedTransaction) => console.log('Received transaction', receivedTransaction));
     //listen for new generated address
-    baseWallet.onGenerateAddress((addressHex) => console.log('Generated address', addressHex));
+    wallet.onGenerateAddress((addressHex) => console.log('Generated address', addressHex));
 
-    /* initiate BaseAddress instance to load to BaseWallet instance.
+    /* initiate Address instance to load to Wallet instance.
        By default, the preBalance and balance are initiated by 0. If
        you store the address with its balances at your DB (which is
        strongly recommended), then you can set those balances with:
@@ -81,27 +81,26 @@ const apiKey = 'yourApiKey';
        every time checkBalancesOfAddresses method is called, you will
        get all the address balances at onBalanceChange.
     */
-    // const baseAddress = new BaseAddress(address);
-    const address = await baseWallet.generateAddressByIndex(addressIndex);
-    // loading the addresses to baseWallet
-    await baseWallet.loadAddresses([address]);
+    const address = await wallet.generateAddressByIndex(addressIndex);
+    // loading the addresses to wallet
+    await wallet.loadAddresses([address]);
 
     // Initiate here a transaction from DB that is related with address. This transaction is stored to DB by your app previously.
     let transactionFromDb;
     // createTime and transactionConsensusUpdateTime are timestamp in seconds with milliseconds in decimal points
     const { hash, createTime, transactionConsensusUpdateTime } = transactionFromDb;
-    // transaction to load to baseWallet
+    // transaction to load to wallet
     const reducedTransaction = new ReducedTransaction(hash, createTime, transactionConsensusUpdateTime);
     // load the initial known transactions related with your loaded addresses
-    await baseWallet.loadTransactions([reducedTransaction]);
+    await wallet.loadTransactions([reducedTransaction]);
 
     // sync the balances of native coti for all loaded addresses from network
-    await baseWallet.checkBalancesOfAddresses();
+    await wallet.checkBalancesOfAddresses();
     // sync all loaded transactions related with loaded addresses from network
-    await baseWallet.checkTransactionHistory();
+    await wallet.checkTransactionHistory();
 
     // initiate your websocket instance
-    const webSocket = new WebSocket(baseWallet);
+    const webSocket = new WebSocket(wallet);
     // success callback for WebSocket connection
     const successCallback = async () => console.log('Connection success');
     // reconnect fail callback for WebSocket connection
@@ -112,12 +111,12 @@ const apiKey = 'yourApiKey';
     // when you generate another address, and you want to monitor it, you should do:
     const newAddressIndex = 1;
     // new address
-    const newAddress = await baseWallet.generateAddressByIndex(newAddressIndex)
-    // load the new baseAddress to baseWallet
-    await baseWallet.setAddress(newAddress);
+    const newAddress = await wallet.generateAddressByIndex(newAddressIndex);
+    // load the new address to wallet
+    await wallet.setAddress(newAddress);
     // websocket subscription to the new address. Pay attention that newAddress is hexadecimal string
     webSocket.connectToAddress(newAddress.getAddressHex());
-    // END OF BASEWALLET and WEBSOCKET use
+    // END OF WALLET and WEBSOCKET use
 
     // create input map to spend Coti from address
     const amountSpend = 1;
@@ -128,14 +127,14 @@ const apiKey = 'yourApiKey';
     const destinationAddress = 'destinationAddressInHex';
 
     //checks if fullnode had multi currency hardfork
-    const hardFork = await nodeUtils.isNodeSupportMultiCurrencyApis('mainnet', baseWallet.getFullNode());
+    const hardFork = await nodeUtils.isNodeSupportMultiCurrencyApis('mainnet', wallet.getFullNode());
 
     const transactionProperties = {
       userPrivateKey: userKeyPair.getPrivateKey(),
       inputMap,
       feeAddress: address.getAddressHex(), // fee address can be different from addresses in the input map. Then put the private key of feeAddress at the end of private keys to be used to sign
       destinationAddress,
-      wallet: baseWallet,
+      wallet: wallet,
       network,
     };
 
@@ -143,7 +142,7 @@ const apiKey = 'yourApiKey';
       // get the balances of none native tokens for given addresses array.
       await nodeUtils.getTokenBalances([address.getAddressHex()]);
       // get balances of all wallet addresses from network none native token balances
-      await nodeUtils.getUserTokenCurrencies(userHash, baseWallet, baseWallet.getFullNode());
+      await nodeUtils.getUserTokenCurrencies(userHash, wallet, wallet.getFullNode());
       //multi currency additional properties
       //we will use as an example transfer of native currency
       const cotiCurrencyHash = utils.getCurrencyHashBySymbol('coti');
